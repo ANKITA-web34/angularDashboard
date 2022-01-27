6abf03e3f5d1a10cc2e121db86fd44245c568fd2
Ankita <anki333soul@gmail.com>
2022-01-27 23:46:07 +0530

    setup with gernerate componet and setup header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
+import { DefaultModule } from './layout/default/default.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [ AppComponent ],
+  imports: [ BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatSliderModule, MatButtonModule, DefaultModule ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from 'src/app/shared/components/header/header.component';
+import { SidebarComponent } from 'src/app/shared/components/sidebar/sidebar.component';
+import { FooterComponent } from 'src/app/shared/components/footer/footer.component';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatMenuModule } from '@angular/material/menu';
+
+@NgModule({
+  declarations: [ HeaderComponent, SidebarComponent, FooterComponent ],
+  imports: [ CommonModule, MatDividerModule, MatToolbarModule, MatIconModule, MatButtonModule, FlexLayoutModule, MatMenuModule ],
+  exports: [ HeaderComponent, SidebarComponent, FooterComponent ]
+})
+export class SharedModule { }
